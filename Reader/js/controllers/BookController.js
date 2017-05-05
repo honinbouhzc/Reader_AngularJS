c15d70f043f5ece484b678c08df64a7dbde67d65
Zhichao Han <zhchhan@gmail.com>
2017-05-05 14:40:19 +1000

    first commit

---
+++ Reader/js/controllers/BookController.js
@@ -0,0 +1,6 @@
+app.controller('BookController', ['$scope', '$routeParams', 'books', function($scope, $routeParams, books) {
+  books.success(function(data) {
+    $scope.book = data[$routeParams.bookId];
+  });
+  $scope.currentBookIndex = parseInt($routeParams.bookId);
+}]);
